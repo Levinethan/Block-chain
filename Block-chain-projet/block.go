0ca389a8f1e9a9d103687af35ae613e57f734ccc
Ethan <48085482+Levinethan@users.noreply.github.com>
2019-10-10 00:45:59 +0800

    代码优化join

--- Block-chain-projet/block.go
+++ Block-chain-projet/block.go
@@ -1,14 +1,30 @@
 package main
 
-import "crypto/sha256"
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"log"
+	"time"
+)
 
 //1.定义区块结构
 type Block struct {
-	//1.前区块hash
+	//1.版本号
+	Version uint64
+	//2.Merkel根
+	MerkelRoot []byte
+	//3.时间戳
+	TimeStamp uint64
+	//4.难度梯度
+	Difficulty uint64
+	//5.随机数 噪音
+	Nonce uint64
+	//6.前区块hash
 	PrevHash []byte
-	//2.当前区块HASH
+	//当前区块HASH
 	Hash []byte
-	//3.区块数据
+	//区块数据
 	Data []byte
 }
 
@@ -18,6 +34,11 @@ func NewBlock(data string,prevBlockHash []byte) *Block  {
 		PrevHash: prevBlockHash,
 		Hash:     []byte{},
 		Data:     []byte(data),
+		Version:00,
+		MerkelRoot:[]byte{},
+		TimeStamp:uint64(time.Now().Unix()),
+		Nonce:0,               //随机值  无法知道区块阈值
+		Difficulty:0,        //无效值   无法知道困难梯度
 	}
 	block.SetHash()
 	return &block
@@ -26,9 +47,43 @@ func NewBlock(data string,prevBlockHash []byte) *Block  {
 
 //3.生成HASH
 func (block *Block) SetHash()  {
+	//var blockInfo  []byte
 	//拼装数据
-	blockInfo := append(block.PrevHash,block.Data...)
+	/*
+	blockInfo =append(blockInfo,Uint64ToByte(block.Version)...)
+	blockInfo =append(blockInfo,block.PrevHash...)
+	blockInfo =append(blockInfo,block.MerkelRoot...)
+	blockInfo =append(blockInfo,Uint64ToByte(block.TimeStamp)...)
+	blockInfo =append(blockInfo,Uint64ToByte(block.Difficulty)...)
+	blockInfo =append(blockInfo,Uint64ToByte(block.Nonce)...)
+	blockInfo =append(blockInfo,block.Data...)
+
+	 */ //优化 用join函数
+	 tmp := [][]byte{
+		 Uint64ToByte(block.Version),
+		 block.PrevHash,
+		 block.MerkelRoot,
+		 Uint64ToByte(block.TimeStamp),
+		 Uint64ToByte(block.Difficulty),
+		 Uint64ToByte(block.Nonce),
+		 block.Data,
+
+	 }
+	 blockInfo :=bytes.Join(tmp,[]byte{})
+	 //将二维的数组链接起来，返回一个一维的切片
+
+
 	//sha256加密
 	hash := sha256.Sum256(blockInfo)
 	block.Hash = hash[:]
+}
+//4.实现一个辅助函数 用来转换数据类型
+func Uint64ToByte(num uint64 ) []byte {      //func XXX()括号里面 传入参数 []byte 返回一个byte类型数据
+	var buffer  bytes.Buffer
+	err := binary.Write(&buffer,binary.BigEndian,num)
+	if err != nil{
+		log.Panic(err)
+	}
+	return buffer.Bytes()
+
 }
